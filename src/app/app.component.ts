27a71cf55544842925cc8953d55bbbcd3b38308e
unknown <iankim2280@gmail.com>
2020-12-01 14:18:00 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// Component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
+// Logic
 export class AppComponent {
   title = 'my-first-project';
 }
